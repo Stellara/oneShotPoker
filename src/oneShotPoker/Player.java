5145c4bc18dd2bf8ef74c08498aad4ee3de8d9a2
EK <erinsmith.8585@gmail.com>
2019-12-09 16:46:52 -0500

    Each player should have their own hand of cards

--- src/oneShotPoker/Player.java
+++ src/oneShotPoker/Player.java
@@ -1,36 +1,24 @@
 package oneShotPoker;
 
+import java.util.ArrayList;
+
 public class Player {
     private boolean isWinner;
+    private ArrayList<oneShotPoker.Card> currentHandOfCards = new ArrayList<>();
 
-    //TODO: there is a much more elegant way to implement this, refactor
     public void setWinner(boolean wonLastGame) {
         this.isWinner = wonLastGame;
     }
 
     public boolean isWinner() {
-        return isWinner;
+        return this.isWinner;
     }
 
-    //public handOfCards getHandOfCards(){
-//        return handOfCards;
-//        }
-//
-//public handOfCards setHand(handOfCards newHand){
-//        this.handOfCards = newHand;
-//        }
+    public ArrayList<Card> getCurrentHandOfCards(){
+        return this.currentHandOfCards;
+    }
+
+    public void setCurrentHandOfCards(ArrayList<Card> newHand){
+        this.currentHandOfCards = newHand;
+    }
 }
-
-
-
-//
-//public handOfCards getHandOfCards(){
-//        return handOfCards;
-//        }
-//
-//public handOfCards setHand(handOfCards newHand){
-//        this.handOfCards = newHand;
-//        }
-
-
-//oneShotPoker.GameRunner will set the player's hand, just need to declare here that they have one
